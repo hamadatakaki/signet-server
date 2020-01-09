c145fff1b43af0449c749727bfc48322ef9bc061
hamadatakaki <i16hamadat@gmail.com>
2020-01-09 14:55:03 +0900

    Add dummy endpoint

--- server/views/view.py
+++ server/views/view.py
@@ -3,3 +3,22 @@ from server.models import SessionManager
 from server.models.signet import Signet
 
 import json
+
+
+@api.route('/signets')
+class SignetView:
+    def on_get(self, req, resp):
+        resp.media = {'message': 'on_get'}
+        resp.status_code = api.status_codes.HTTP_200
+
+    async def on_post(self, req, resp):
+        data = await req.media()
+        print(data)
+        resp.media = {'message': 'on_post'}
+        resp.status_code = api.status_codes.HTTP_201
+
+    async def on_delete(self, req, resp):
+        data = await req.media()
+        print(data)
+        resp.media = {'message': 'on_delete'}
+        resp.status_code = api.status_codes.HTTP_202
