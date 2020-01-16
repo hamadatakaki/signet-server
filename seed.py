f31a1b42f79925c8ee1dc54d84309275a03c95a1
hamadatakaki <i16hamadat@gmail.com>
2020-01-16 14:50:12 +0900

    Define signet's table

--- seed.py
+++ seed.py
@@ -7,9 +7,16 @@ if __name__=='__main__':
     Base.metadata.create_all(bind=engine)
 
     signets = [
-        Signet(url="https://www.sig.net/new_article/?id=0324", position=1278),
-        Signet(url="https://onsen-musume.jp/character/iizaka_mahiro", position=3456),
-        Signet(url="https://jellyfishrumble.net", position=450),
+        Signet(
+            url="https://www.sig.net/new_article/?id=0324", icon="https://www.sig.net/images/2134.jpg", 
+            title="ふぇ〜、フィッシュアンドチップスがこんなにたくさん...", comment="フッド姉さまが用意してくれたんだ...", position=1278
+        ),
+        Signet(
+            url="https://onsen-musume.jp/character/iizaka_mahiro", 
+            icon="onsen-musume.jp/wp/wp-content/themes/onsenmusume/pc/assets/img/siteinfo/favicon.ico",
+            title="飯坂真尋 | 温泉むすめ公式サイト", comment="飯坂温泉行きたい", position=3456
+        ),
+        Signet(url="https://jellyfishrumble.net", icon="", title="jellyfishrumble.net", comment="クソサイトですまん...", position=450),
     ]
 
     with SessionManager() as session:
